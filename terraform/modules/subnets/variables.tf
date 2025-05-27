bfe07558a073dbb4077c34b87db033d34e30a2c5
Ginikanwa Ikewesi <123885159+gikewesi@users.noreply.github.com>
2025-05-27 16:18:49 -0500

    modified variable

--- terraform/modules/subnets/variables.tf
+++ terraform/modules/subnets/variables.tf
@@ -1,31 +1,29 @@
-
-
-variable "mallhive-public-1a-cidr_block" {
+variable "mallhive_public_1a_cidr_block" {
   description = "CIDR block for public subnet in AZ1"
   type        = string
 }
 
-variable "mallhive-public-1b-cidr_block" {
+variable "mallhive_public_1b_cidr_block" {
   description = "CIDR block for public subnet in AZ2"
   type        = string
 }
 
-variable "mallhive-private-1a-cidr_block" {
+variable "mallhive_private_1a_cidr_block" {
   description = "CIDR block for first private subnet in AZ1"
   type        = string
 }
 
-variable "mallhive-isolated-1a-cidr_block" {
+variable "mallhive_isolated_1a_cidr_block" {
   description = "CIDR block for second private subnet in AZ1"
   type        = string
 }
 
-variable "mallhive-private-1b-cidr_block" {
+variable "mallhive_private_1b_cidr_block" {
   description = "CIDR block for first private subnet in AZ2"
   type        = string
 }
 
-variable "mallhive-isolated-1b-cidr_block" {
+variable "mallhive_isolated_1b_cidr_block" {
   description = "CIDR block for second private subnet in AZ2"
   type        = string
 }
